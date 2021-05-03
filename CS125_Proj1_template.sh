20c69869dcc6da5aa7f17ca9f16293e7291eda43
Jeremy Harding <75227382+whatdoesthismean@users.noreply.github.com>
2021-05-02 17:56:59 -0700

    good try
    lemme see

---
+++ CS125_Proj1_template.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+###
+#perform PROJECT1 for CS125: Create user account and assign password from words.txt.
+#Coded by: <your name>
+###
+
+read -p "Please run this script using sudo, else will not assign password to users and cannot copy shadow file to Sandbox. Press Enter key to proceed."
+
+cd ~
+
+##
+#Create sandbox directory in home
+<your command is here>
+##
+
+##
+#Download password file online
+##
+wget <url> -O <filename>
+
+
+##From passwordlist.txt, shuffle and randomly select 100 entries then save it to words.txt in the sandbox directory
+
+#cd to the sandbox
+<your command is here>
+
+##
+#create5  users, generate and assign password from words.txt 
+##
+<your command is here>
+
+
+
+##
+#copy shadow file from /etc/shadow to sandbox directory
+##
+<your command is here>
